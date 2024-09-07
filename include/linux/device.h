8e1b4d3fa7cee6e791ebe046214f59d2b2620b94
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2024-09-07 04:11:44 +0000

    add iommu field
    where is it initialized?

--- include/linux/device.h
+++ include/linux/device.h
@@ -1051,6 +1051,7 @@ struct device {
 
 	void	(*release)(struct device *dev);
 	struct iommu_group	*iommu_group;
+    struct dev_iommu	*iommu;
 	struct iommu_fwspec	*iommu_fwspec;
 
 	bool			offline_disabled:1;
@@ -1299,6 +1300,8 @@ static inline void *dev_get_platdata(const struct device *dev)
  * Manual binding of a device to driver. See drivers/base/bus.c
  * for information on use.
  */
+int __must_check device_driver_attach(struct device_driver *drv,
+				      struct device *dev);
 extern int __must_check device_bind_driver(struct device *dev);
 extern void device_release_driver(struct device *dev);
 extern int  __must_check device_attach(struct device *dev);
